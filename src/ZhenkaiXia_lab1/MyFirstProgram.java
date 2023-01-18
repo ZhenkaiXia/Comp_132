6bc612a8bd77a81c2648654af5050cd652831e10
zhenkai laptop <zhenkaix7156@gmail.com>
2023-01-18 11:31:17 -0800

    lab 1 completed

---
+++ src/ZhenkaiXia_lab1/MyFirstProgram.java
@@ -0,0 +1,23 @@
+package ZhenkaiXia_lab1;
+
+/**
+ *
+ * @author Zhenkai Xia
+ */
+public class MyFirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(
+                "Name: Zhenkai Xia\n"
+                        + "I am taking this couse for the credit in order to transfer "
+                        + "into computer science at "
+                        + "Uvic in the future.\n"
+                        + "I hope to learn more about polymorphism and creating "
+                        + "objects."
+        );
+    }
+
+}
