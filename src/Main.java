6095002f7fe5e41cff97ee658f15bc4310dbb597
zhenkai laptop <zhenkaix7156@gmail.com>
2023-01-23 13:49:48 -0800

    lab 1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
+
 public class Main {
+    // CCC 2022 q 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        test();
+
     }
 
-    /**
-     * Testing
-     *
-     */
-    public static void test() {
-        System.out.println("test success");
-    }
+
 }
